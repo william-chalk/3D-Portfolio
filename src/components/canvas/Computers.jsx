caad209d9f94265d87581d676e8dd4d1e46c498d
William Chalk <3halk1@gmail.com>
2023-03-05 14:51:22 -0500

    mobile responsive hero

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -31,11 +31,27 @@ const computer = useGLTF('./desktop_pc/scene.gltf');
 }
 
 const ComputersCanvas = () => {
+  const [isMobile,setIsMobile] = useState(false);
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+
+    return () =>{
+      mediaQuery.removeEventListener('change',handleMediaQueryChange);
+    }
+  })
+
   return(
     <Canvas frameloop='demand' shadows camera={{position:[20,3,5],fov:25}} gl={{preserveDrawingBuffer:true}}>
       <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all/>
     </Canvas>
