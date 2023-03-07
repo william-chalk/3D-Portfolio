15496f198555913b3fdcf5a94803998ad3bb5ee8
William Chalk <3halk1@gmail.com>
2023-03-06 20:11:02 -0500

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form,setForm] = useState({
@@ -16,9 +17,58 @@ const Contact = () => {
   })
   const [loading,setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
-  const handleSubmit = (e) => {}
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    //bkzd77OfB5-ErDKvq
+
+    //service_9xij4cn
+
+    //template_opbm5ig
+
+    emailjs
+      .send(
+        'service_9xij4cn',
+        'template_opbm5ig',
+        {
+          from_name: form.name,
+          to_name: "William Chalk",
+          from_email: form.email,
+          to_email: "3halk1@gmail.com",
+          message: form.message,
+        },
+        'bkzd77OfB5-ErDKvq'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
